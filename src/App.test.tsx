63bd4904f7ba924a6e65a8042d3e140f37401687
Pierce <spammermuch@gmail.com>
2024-03-24 22:34:12 -0700

    Updates

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Pierce\'s name', () => {
-  render(<App />);
-  const name = screen.getByText(/Pierce Harriz/i);
-  expect(name).toBeInTheDocument();
-});
